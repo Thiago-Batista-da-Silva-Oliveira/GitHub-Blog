e56a79f1088c011e903609a2fd8ef21b93429a74
Thiago <gmoliveira69@gmail.com>
2022-09-01 23:35:32 -0300

    first commit

---
+++ github-blog/src/App.tsx
@@ -0,0 +1,19 @@
+import {BrowserRouter} from 'react-router-dom'
+import { Router } from './Router'
+import {ThemeProvider} from 'styled-components'
+import { defaultTheme } from './styles/theme/defaultTheme'
+
+export function App() {
+
+  return (
+    <>
+     <BrowserRouter>
+       <ThemeProvider theme={defaultTheme}>
+         <Router />
+       </ThemeProvider>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
